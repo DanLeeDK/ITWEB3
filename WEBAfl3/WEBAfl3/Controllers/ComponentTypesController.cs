70037d827136902c5f37e5a404242bf55a696859
Dan Lee <danjhinlee@hotmail.com>
2018-11-13 15:15:35 +0100

    ComponentType

---
+++ WEBAfl3/WEBAfl3/Controllers/ComponentTypesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WEBAfl3.Data;
+using WEBAfl3.Models;
+
+namespace WEBAfl3.Controllers
+{
+    public class ComponentTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ComponentTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ComponentTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ComponentTypes.ToListAsync());
+        }
+
+        // GET: ComponentTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var componentType = await _context.ComponentTypes
+                .FirstOrDefaultAsync(m => m.ComponentTypeId == id);
+            if (componentType == null)
+            {
+                return NotFound();
+            }
+
+            return View(componentType);
+        }
+
+        // GET: ComponentTypes/Create
+        public IActionResult Create()
+        {
+            var componentType = new ComponentType();
+            return View(componentType);
+        }
+
+        // POST: ComponentTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ComponentTypeId,ComponentName,ComponentInfo,Location,Status,Datasheet,ImageUrl,Manufacturer,WikiLink,AdminComment")] ComponentType componentType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(componentType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(componentType);
+        }
+
+        // GET: ComponentTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var componentType = await _context.ComponentTypes.FindAsync(id);
+            if (componentType == null)
+            {
+                return NotFound();
+            }
+            return View(componentType);
+        }
+
+        // POST: ComponentTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ComponentTypeId,ComponentName,ComponentInfo,Location,Status,Datasheet,ImageUrl,Manufacturer,WikiLink,AdminComment")] ComponentType componentType)
+        {
+            if (id != componentType.ComponentTypeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(componentType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ComponentTypeExists(componentType.ComponentTypeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(componentType);
+        }
+
+        // GET: ComponentTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var componentType = await _context.ComponentTypes
+                .FirstOrDefaultAsync(m => m.ComponentTypeId == id);
+            if (componentType == null)
+            {
+                return NotFound();
+            }
+
+            return View(componentType);
+        }
+
+        // POST: ComponentTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var componentType = await _context.ComponentTypes.FindAsync(id);
+            _context.ComponentTypes.Remove(componentType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ComponentTypeExists(int id)
+        {
+            return _context.ComponentTypes.Any(e => e.ComponentTypeId == id);
+        }
+    }
+}
